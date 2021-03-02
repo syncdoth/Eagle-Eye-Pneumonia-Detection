1e3496e36851d7fadd64d1d8507b6f9e77de38e2
Choi Sehyun <choisehyun98@gmail.com>
2021-03-02 23:41:22 +0900

    Update ClassificationDataset, handle some errrors,update sample code

--- dataloader/ClassificationDataset.py
+++ dataloader/ClassificationDataset.py
@@ -7,6 +7,7 @@ from PIL import Image
 import pydicom
 
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 from torchvision import transforms
 
 
@@ -19,36 +20,47 @@ class ClassificationDataset(torch.utils.data.Dataset):
     labels: [1,] torch.Tensor. each image's label.
         Pneumonia = 1, Covid = 2, normal = 0
     """
-    def __init__(self, root, mode="train", trans=None):
+    def __init__(self, root="data_server", split=[0.8, 0.1, 0.1], trans=None):
         self.root = root
-        self.mode = mode
+        self.split = split
         self.trans = trans
+        self.class2id = {
+            'Normal': 0,
+            'Not Normal': 0,
+            'COVID19-PN': 1,
+            'TheOther-PN': 2,
+            'Viral-PN': 3
+        }
 
-        self.data_path = pd.read_csv(os.path.join(root, f"data_{mode}.csv"))
-        self.imgs = list(dict.fromkeys(self.data_path["filename"].to_list()))
+        self.data_path = pd.read_csv(os.path.join(root, "class/meta/metadata.csv"))
+        self.data_path = self.data_path[self.data_path["CLASS"] != "Unclassified-PN"]
+
+        self.data_path["IMG_PATH"] = self.data_path["FILE_PATH"].map(
+            lambda x: x[2:]) + self.data_path["FILE NAME"]
+        self.imgs = self.data_path["IMG_PATH"].to_numpy()
+        self.labels = self.data_path["CLASS"].map(lambda x: self.class2id[x]).to_numpy()
+
+        self.train_idx, self.val_idx, self.test_idx = self.dataset_split()
 
     def __len__(self):
-        return len(self.imgs)
+        return self.imgs.shape[0]
 
     def __getitem__(self, idx):
-        # load images
-        img_path = os.path.join(self.root, self.mode, self.imgs[idx])
+        # load image and label
+        img_path = os.path.join(self.root, self.imgs[idx])
         img = load_img(img_path)
+        label = self.labels[idx]
 
         # normalize to [0, 1]
         scaler = MinMaxScaler()
-        img = scaler.fit_transform(img.reshape(-1, img.shape[-1])).reshape(img.shape)
+        img = scaler.fit_transform(img.reshape(-1, img.shape[0])).reshape(img.shape)
         img = torch.Tensor(img)
-
-        # load bbox and label
-        objs = self.data_path[self.data_path['filename'] == self.imgs[idx]]
-        labels = objs["label"].to_numpy()
 
         # data augmentation
         target_transform = transforms.Compose([
             # TODO: Add resize, change it to actual resnet norm, and add more augmentations
             transforms.Resize([224, 224]),
-            # use the first channel as last channel again
+            # use actual spec
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
         if self.trans is not None:
@@ -59,9 +71,23 @@ class ClassificationDataset(torch.utils.data.Dataset):
         else:
             train_transform = target_transform
 
-        img = train_transform(img) if self.mode == "train" else target_transform(img)
+        img = train_transform(img) if idx in self.train_idx else target_transform(img)
 
-        return img, labels
+        return img, label
+
+    def dataset_split(self):
+        labels = self.labels
+        train_idx, val_idx = train_test_split(np.arange(len(labels)),
+                                              test_size=self.split[1] + self.split[2],
+                                              shuffle=True,
+                                              stratify=labels)
+        val_idx, test_idx = train_test_split(np.arange(len(val_idx)),
+                                             test_size=self.split[2] /
+                                             (self.split[1] + self.split[2]),
+                                             shuffle=True,
+                                             stratify=labels[val_idx])
+
+        return train_idx, val_idx, test_idx
 
 
 # TODO: move to utils.
@@ -71,13 +97,21 @@ def load_img(image_path):
     Args:
         image_path (String): a string path to the image.
     """
-    ext = os.path.basename(image_path).split(".")[1]
+    ext = os.path.basename(image_path).split(".")[-1]
     if ext == "dcm":
         image = pydicom.read_file(image_path)
         image = image.pixel_array
-    elif ext in ["png", "jpg"]:
+
+    elif ext in ["png", "jpg", "jpeg"]:
         image = Image.open(image_path)
         image = np.array(image)
+
     else:
         raise ValueError(f"the image has unsupprted extension: {ext}")
+
+    if len(image.shape) == 2:
+        image = np.repeat(image[:, :, np.newaxis], 3,
+                          axis=2)  # make it 3 channel by repeating
+
+    image = image.transpose(2, 0, 1)  # [C, H, W]
     return image
--- models/train.py
+++ models/train.py
@@ -1,3 +1,5 @@
+import numpy as np
+import torch
 from tqdm.auto import tqdm
 from sklearn.metrics import f1_score
 
@@ -15,15 +17,14 @@ def make_batch(batch):
 
 def train(model, optimizer, train_dataset, device, loss_func, val_dataset=None, epochs=1):
     """Outer training loop"""
-    history = dict(train_f1=[],train_loss=[],val_f1=[],val_loss=[])
-
+    history = dict(train_f1=[], train_loss=[], val_f1=[], val_loss=[])
 
     for epoch in range(epochs):
         train_loss = []
         train_f1 = []
 
         model.train(True)
-        train_bar = tqdm(train_dataset, desc=f'[train {epoch+1}/{epochs}]')
+        train_bar = tqdm(train_dataset, desc=f"[train {epoch+1}/{epochs}]")
 
         for batch in train_bar:
             for key in batch.keys():
@@ -31,19 +32,20 @@ def train(model, optimizer, train_dataset, device, loss_func, val_dataset=None, 
 
             loss, f1 = do_train_step(model, optimizer, loss_func, batch)
 
-            print(f"current loss: {loss}, current f1_score: {f1}",end=' /')
+            print(f"current loss: {loss}, current f1_score: {f1}", end=" /")
             train_loss.append(loss)
             train_f1.append(f1)
 
         train_loss = np.mean(train_loss)
         train_f1 = np.mean(train_f1)
-        history['train_loss'].append(train_loss)
-        history['train_f1'].append(train_f1)
+        history["train_loss"].append(train_loss)
+        history["train_f1"].append(train_f1)
 
         ### valid
 
         if val_dataset is None:
-            save_checkpoint(epoch,model,optimizer,'{}_{}_model'.format(epoch,train_loss))#save_checkpoint
+            save_checkpoint(epoch, model, optimizer,
+                            "{}_{}_model".format(epoch, train_loss))  #save_checkpoint
             continue
 
         val_loss = []
@@ -55,7 +57,11 @@ def train(model, optimizer, train_dataset, device, loss_func, val_dataset=None, 
                 for key in batch.keys():
                     batch[key] = batch[key].to(device)
 
-                loss, f1 = do_train_step(model, optimizer, loss_func, batch, is_train=False)
+                loss, f1 = do_train_step(model,
+                                         optimizer,
+                                         loss_func,
+                                         batch,
+                                         is_train=False)
                 print(f"validation: current loss: {loss}, current f1_score: {f1}")
 
                 val_loss.append(loss)
@@ -63,10 +69,11 @@ def train(model, optimizer, train_dataset, device, loss_func, val_dataset=None, 
 
         val_loss = np.mean(val_loss)
         val_f1 = np.mean(val_f1)
-        history['val_loss'].append(val_loss)
-        history['val_f1'].append(val_f1)
+        history["val_loss"].append(val_loss)
+        history["val_f1"].append(val_f1)
 
-        save_checkpoint(epoch,model,optimizer,'{}_{}_model'.format(epoch,val_loss)) # save_checkpoint
+        save_checkpoint(epoch, model, optimizer,
+                        "{}_{}_model".format(epoch, val_loss))  # save_checkpoint
 
     return history
 
@@ -78,23 +85,20 @@ def do_train_step(model, optimizer, loss_func, batch, is_train=True):
     # TODO: Add metric calculation
     loss = loss_func(class_score, batch["targets"])
 
-#     predicted = torch.max(class_score.data, 1)[1]
-#     batch_corr = (predicted == batch["targets"]).sum()
-#     get accuracy
-
-    f1_socre = f1_score(batch["targets"], predicted, average='macro')
+    predicted = torch.max(class_score.data, 1)[1]
+    f1 = f1_score(batch["targets"], predicted, average="macro")
 
     if is_train:
         loss.backward()
         optimizer.step()
 
-    return loss, f1_score
+    return loss, f1
 
 
 def save_checkpoint(epoch, model, optimizer, filename):
     state = {
-    'Epoch' = epoch,
-    'State_dict' = model.state_dict(),
-    'optimizer' = optimizer.state_dict()
+        "Epoch": epoch,
+        "State_dict": model.state_dict(),
+        "optimizer": optimizer.state_dict()
     }
-    torch.save(state,filename)
+    torch.save(state, filename)
