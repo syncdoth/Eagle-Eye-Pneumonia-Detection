e4d7970f09f79b3349e6b8533f2a6d4ed50e23f6
Choi Sehyun <choisehyun98@gmail.com>
2021-03-23 20:10:18 +0900

    load and save from checkpoint + better logging

--- models/train.py
+++ models/train.py
@@ -1,8 +1,9 @@
-import numpy as np
+import os
+
 import torch
 from tqdm.auto import tqdm
-from sklearn.metrics import f1_score
 from sklearn.metrics import classification_report
+
 
 def make_batch(batch):
     """Used as collate_fn in pytorch Dataloader."""
@@ -15,12 +16,26 @@ def make_batch(batch):
     return {"images": torch.stack(images, 0), "targets": torch.cat(targets)}
 
 
-def train(model, optimizer, train_dataset, device, loss_func, val_dataset=None, epochs=1):
+def train(model,
+          optimizer,
+          train_dataset,
+          device,
+          loss_func,
+          val_dataset=None,
+          epochs=1,
+          model_save_name=None):
     """Outer training loop"""
     history = dict(train_acc=[], train_loss=[], val_acc=[], val_f1=[], val_loss=[])
     curr_best_val_acc = 0
 
-    for epoch in range(epochs):
+    if os.path.exists(model_save_name):
+        checkpoint = torch.load(model_save_name)
+        model.load_state_dict(checkpoint["State_dict"])
+        optimizer.load_state_dict(checkpoint["optimizer"])
+        resuming_epoch = checkpoint["Epoch"]
+        print(f"resuming training from epoch {resuming_epoch + 1}...")
+
+    for epoch in range(resuming_epoch + 1, epochs):
 
         model.train(True)
         train_bar = tqdm(train_dataset, desc=f"[train {epoch + 1}/{epochs}]")
@@ -35,25 +50,20 @@ def train(model, optimizer, train_dataset, device, loss_func, val_dataset=None, 
 
             pred, loss = do_train_step(model, optimizer, loss_func, batch)
             running_loss += loss.item()
-            correct_num += (pred == batch['targets']).sum().item()
-            total_num += batch['targets'].shape[0]
+            correct_num += (pred == batch["targets"]).sum().item()
+            total_num += batch["targets"].shape[0]
 
             train_loss = running_loss / total_num
             train_acc = correct_num / total_num
             history["train_loss"].append(train_loss)
             history["train_acc"].append(train_acc)
             train_bar.set_postfix(loss=train_loss, acc=train_acc)
-        print(
-            f"[train] epoch: {epoch}, loss: {train_loss},"
-            f" accracy: {train_acc}")
-
-
+        print(f"[train] epoch: {epoch}, loss: {train_loss}," f" accracy: {train_acc}")
 
         ### valid
 
         if val_dataset is None:
-            save_checkpoint(epoch, model, optimizer,
-                            "{}_{}_model".format(epoch, train_loss))  #save_checkpoint
+            save_checkpoint(epoch, model, optimizer, model_save_name)  #save_checkpoint
             continue
 
         val_running_loss = 0
@@ -61,32 +71,33 @@ def train(model, optimizer, train_dataset, device, loss_func, val_dataset=None, 
         y_true = []
         model.eval()
         with torch.no_grad():
-            for batch in val_dataset:
+            val_bar = tqdm(val_dataset, desc=f"[val {epoch + 1}/{epochs}]")
+            for batch in val_bar:
                 for key in batch.keys():
                     batch[key] = batch[key].to(device)
 
                 pred, loss = do_train_step(model,
-                                         optimizer,
-                                         loss_func,
-                                         batch,
-                                         is_train=False)
+                                           optimizer,
+                                           loss_func,
+                                           batch,
+                                           is_train=False)
                 val_running_loss += loss.item()
                 y_pred.extend(pred.tolist())
-                y_true.extend(batch['targets'].tolist())
+                y_true.extend(batch["targets"].tolist())
 
         report = classification_report(y_true, y_pred, output_dict=True)
         val_loss = val_running_loss / len(val_dataset)
         history["val_loss"].append(val_loss)
-        history["val_acc"].append(report['accuracy'])
-        history["val_f1"].append(report['macro avg']['f1-score'])
-        print(
-            f"[valid] epoch: {epoch}, loss: {val_loss},"
-            f" report:\n{report}")
+        history["val_acc"].append(report["accuracy"])
+        history["val_f1"].append(report["macro avg"]["f1-score"])
+        print(f"[valid] epoch: {epoch}, loss: {val_loss},"
+              f" report:\n{classification_report(y_true, y_pred)}")
         if report["accuracy"] > curr_best_val_acc:
             curr_best_val_acc = report["accuracy"]
-            save_checkpoint(epoch, model, optimizer, "model.pth")  # save_checkpoint
+            save_checkpoint(epoch, model, optimizer, model_save_name)  # save_checkpoint
 
     return history
+
 
 def do_train_step(model, optimizer, loss_func, batch, is_train=True):
     """inner training step"""
@@ -95,7 +106,6 @@ def do_train_step(model, optimizer, loss_func, batch, is_train=True):
     loss = loss_func(class_score, batch["targets"])
 
     predicted = torch.max(class_score.data, 1)[1]
-
 
     if is_train:
         loss.backward()
@@ -108,6 +118,6 @@ def save_checkpoint(epoch, model, optimizer, filename):
     state = {
         "Epoch": epoch,
         "State_dict": model.state_dict(),
-        "optimizer": optimizer.state_dict()
+        "optimizer": optimizer.state_dict(),
     }
     torch.save(state, filename)
