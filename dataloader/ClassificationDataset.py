dc4613f8ee207bcd04810c8501d380ce7e21fe6e
leonilpark <pjangh98@naver.com>
2021-03-27 18:35:09 +0900

    argumentation update

--- dataloader/ClassificationDataset.py
+++ dataloader/ClassificationDataset.py
@@ -9,9 +9,18 @@ from sklearn.utils.class_weight import compute_class_weight
 from torchvision import transforms
 
 from utils.utils import load_img
-import torchvision
 
-
+class AddGaussianNoise(object):
+    def __init__(self, mean=0., std=1.):
+        self.std = std
+        self.mean = mean
+        
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+    
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+        
 class ClassificationDataset(torch.utils.data.Dataset):
     """Usage:
 
